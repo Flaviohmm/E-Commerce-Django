a252137f2af5d8b1d6012dcbd7290063889055ea
Flaviohmm <fhenrique609@gmail.com>
2021-12-13 11:21:15 -0300

    Form de Contato no Template

--- core/forms.py
+++ core/forms.py
@@ -4,4 +4,9 @@ class ConatctForm(forms.Form):
     name = forms.CharField(label='Nome')
     email = forms.EmailField(label='E-mail')
     message = forms.CharField(label='Mensagem', widget=forms.Textarea)
-    
+
+    # def __init__(self, *args, **kwargs):
+    #     super(ConatctForm, self).__init__(*args, **kwargs)
+    #     self.fields['name'].widget.attrs['class'] = 'form-control'
+    #     self.fields['email'].widget.attrs['class'] = 'form-control'
+    #     self.fields['message'].widget.attrs['class'] = 'form-control'
--- core/views.py
+++ core/views.py
@@ -1,14 +1,23 @@
 # coding=utf-8
 
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .forms import ConatctForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-def contact(request):
-    return render(request, 'contact.html')
 
-def product(request):
-    return render(request, 'product.html')
+def contact(request):
+    if request.method == 'POST':
+        form = ConatctForm(request.POST)
+    else:
+        form = ConatctForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contact.html', context)
