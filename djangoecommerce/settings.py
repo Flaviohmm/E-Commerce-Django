57158a926ec8df4109015d7983a8d3d9acdb7c2a
Flaviohmm <fhenrique609@gmail.com>
2021-12-30 11:23:06 -0300

    Login com E-mail

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -158,6 +158,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.ModelBackend',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
