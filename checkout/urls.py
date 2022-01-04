3343dd062b7bcc657c1d73a607a8df0fe5a428ba
Flaviohmm <fhenrique609@gmail.com>
2022-01-04 09:37:20 -0300

    Página do carrinho de compras

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'checkout'
 
 urlpatterns = [
-    re_path(r'^carrinho/adicionar/(?P<slug>[\w_-]+)/$', views.create_cartitem, name='create_cartitem')
+    re_path(r'^carrinho/adicionar/(?P<slug>[\w_-]+)/$', views.create_cartitem, name='create_cartitem'),
+    re_path(r'^carrinho/$', views.cart_item, name='cart_item')
 ]
